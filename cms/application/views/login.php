da5192dc9c2d5868dd428380763f8f21be37f826
miyaseciek <miyase.cicek.0077@gmail.com>
2018-09-03 11:36:57 +0300

    Miyase

--- cms/application/views/login.php
+++ cms/application/views/login.php
@@ -6,6 +6,9 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Giriş Ekranı Sayfası</title>
+    <scprit src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></scprit>
+    <scprit src="<?php echo base_url('elz/login.js');?>"></scprit>
+    <link rel="stylesheet" href="<?php echo base_url('elz/login.css');?>">
 </head>
 <body>
 <div class="wrapper">
