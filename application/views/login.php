31283da0ddc7c7a41eb1220a4363f22a91b29937
miyaseciek <miyase.cicek.0077@gmail.com>
2018-09-05 11:15:49 +0300

    Miyase

--- application/views/login.php
+++ application/views/login.php
@@ -6,7 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Giriş Ekranı Sayfası</title>
-    <scprit src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></scprit>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <scprit src="<?php echo base_url('elz/login.js');?>"></scprit>
     <link rel="stylesheet" href="<?php echo base_url('elz/login.css');?>">
 </head>
@@ -14,17 +17,10 @@
 <div class="wrapper">
     <div class="container">
         <h1>Hoşgeldin</h1>
-
         <form class="form">
-            <input type="text" placeholder="Kullanıcı Adı">
-            <input type="password" placeholder="Şifre">
-            <button style="margin: 5px;" type="submit" id="login-button">Giriş</button><br/><br/>
-            <button style="margin: 2px; width: 100px; font-family: Georgia1, Georgia, serif;" type="submit" id="login-button">Facebook</button>
-            <button style="margin: 2px; width: 100px; font-family: Times New Roman, Times New Roman, Times, serif;" type="submit" id="login-button">Google+</button>
-            <button style="margin: 2px; width: 100px; font-family: Palatino Linotype, Book Antiqua3, Palatino, serif;" type="submit" id="login-button">Instagram</button>
+            <button type="submit" id="login-button">Login</button>
         </form>
     </div>
-
     <ul class="bg-bubbles">
         <li></li>
         <li></li>
@@ -35,8 +31,50 @@
         <li></li>
         <li></li>
         <li></li>
-        <li></li>
     </ul>
+    <div id="buttons">
+        <div class="facebook button">
+            <i class="icon">
+                <i class="fa fa-facebook">
+                </i>
+            </i>
+            <div class="slide">
+                <p>
+                    facebook
+                </p>
+            </div>
+            <iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Freference%2Fplugins%2Flike&send=false&layout=button_count&width=80&show_faces=false&font&colorscheme=light&action=like&height=20&appId=568581339861351" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:80px; height:20px;" allowTransparency="true">
+            </iframe>
+        </div>
+
+        <div class="google button">
+            <i class="icon">
+                <i class="fa fa-google-plus">
+                </i>
+            </i>
+            <div class="slide">
+                <p>
+                    google+
+                </p>
+            </div>
+            <!-- Place this tag where you want the +1 button to render. -->
+            <div class="g-plusone" data-size="large">
+            </div>
+            <!-- Place this tag after the last +1 button tag. -->
+            <script type="text/javascript">
+                (function() {
+                        var po = document.createElement('script');
+                        po.type = 'text/javascript';
+                        po.async = true;
+                        po.src = 'https://apis.google.com/js/plusone.js';
+                        var s = document.getElementsByTagName('script')[0];
+                        s.parentNode.insertBefore(po, s);
+                    }
+                )();
+            </script>
+        </div>
+    </div>
+
 </div>
 </body>
 </html>
