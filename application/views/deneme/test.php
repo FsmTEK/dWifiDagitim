a683ed46ea98ed79a63757e77f96de683058bc58
miyaseciek <miyase.cicek.0077@gmail.com>
2018-10-16 11:27:03 +0300

    miyase

--- application/views/deneme/test.php
+++ application/views/deneme/test.php
@@ -6,45 +6,54 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Deneme Konu</title>
-<style></style>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
 
 </head>
 <body>
-
-    <select name="test" class="form" style="background-color: white; width: 200px; height: 20px;">
-        <option id="1" value="kurumsal" name="kurum"> Kurumsal </option>
-        <option id="2" value="sahis" name="sahis"> Şahıs </option>
-    </select>
-    <br>
-    <br>
-    <br>
-
-<div>
-    <form action="" method="post">
-        Adı:   <input type="text">
-        Soyadı:<input type="text">
-        T.c No:<input type="text">
-    </form>
-</div>
-
-    <br>
-    <br>
-    <br>
-
-<div>
-    <form action="" method="post">
-        Kurum Adı:<input type="text">
+<label> Seciniz: </label>
+<Select id="colorselector" style="background-color: white; width: 100px; height: 30px;">
+    <option value="kurumsal">Kurumsal</option>
+    <option value="sahis">Şahıs</option>
+</Select>
+<br>
+<br>
+<div id="kurumsal" class="colors" style="display:none">
+    <form method="post">
+        Kurum Adı: <input type="text">
         Kişi Adı: <input type="text">
         Vergi No: <input type="text">
     </form>
 </div>
-    <script>
-    $(document).ready(function(){
-    $(".form").click(function(){
-    $("div").toggle();
-    });
-    });
-    </script>
+<div id="sahis" class="colors" style="display:none">
+    <form method="post">
+        Adı:   <input type="text">
+        Soyadı: <input type="text">
+        T.c No: <input type="text">
+    </form>
+</div>
 
+
+
+<script type="text/javascript">
+    $(function() {
+        $('#colorselector').change(function(){
+            $('.colors').hide();
+            $('#' + $(this).val()).show();
+        });
+    });
+</script>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $("select#colorselector").change(function(){
+            var Seciniz = $("#colorselector option:selected").val();
+            alert("Yönlendiriliyorsunuz... - " + Seciniz);
+        });
+    });
+</script>
 </body>
 </html>
