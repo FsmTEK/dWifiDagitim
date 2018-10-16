5cb8747b610ebb0b1b4149e4c6bbae8b321d80ed
miyaseciek <miyase.cicek.0077@gmail.com>
2018-10-16 14:46:24 +0300

    miyase

--- application/controllers/yeni.php
+++ application/controllers/yeni.php
@@ -1,8 +1,18 @@
 <?php
 class yeni extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('test_model');
+    }
 
     public function index()
     {
         $this->load->view('deneme/test');
+    }
+
+    public function miyase()
+    {
+
     }
 }
---
+++ application/models/test_model.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 16.10.2018
+ * Time: 14:42
+ */
+class test_model extends CI_Model{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function kurum()
+    {
+
+    }
+}
